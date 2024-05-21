2d5f4b8104385bc054a7ad5027560aec1f344945
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-05-21 18:04:39 +0000

    replicate_api = st.secrets.replicate['REPLICATE_API_TOKEN']

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="arctic-food-pharmacist")
 # Replicate Credentials
 with st.sidebar:
     if 'REPLICATE_API_TOKEN' in st.secrets:
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+        replicate_api = st.secrets.replicate['REPLICATE_API_TOKEN']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
